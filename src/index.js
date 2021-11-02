a63836792a5e5b2b828a84484de8c41e341f36a3
sergiilevitan <84475017+sergiilevitan@users.noreply.github.com>
2021-11-02 23:30:27 +0300

    ada

--- src/index.js
+++ src/index.js
@@ -1,171 +1,63 @@
-// Example 1 - Базовые математические операторы
-// Выведи на экран общее количество яблок и винограда. Разницу яблок и винограда.
+console.log('module 1.2')
+  
 
-// const apples = 47;
-// const grapes = 135;
-// // const total = ;
-// console.log(total)
-// const diff = ;
-// console.log(diff)
+// // === break===
 
+// for (let i = 0; i < 1000; i += 5) {
+//     // console.log(i);
+//     if (i  === 710) {
+//       console.log(i);
+//         break;
+//     }         
+// }
 
-const apples = 47;
-const grapes = 135;
-const total = apples + grapes ;
-console.log(total);
-const diff =  Math.abs(apples - grapes);
-console.log(diff)
+// // === continue ==
 
-// Example 2 - Комбинированные операторы
-// Замени выражение переопределения комбинированным оператором +=.
+// for (let i = 0; i < 100; i += 1) {
+//     if (!(i % 5 === 0)) {
+//         continue;
+//     }
+//      console.log(i);
+// }
 
-// let students = 100;
-// students = students + 50;
-// console.log(students);
+// // ==== spam check===
+// const checkForSpam = (message) => {
+//     const words = ['spam', 'sale', 'hui'];
+//     const str = message.toLowerCase();
+//     let hasSpam = false;
+//     for (let i = 0; i < words.length; i += 1) {
+//         let word = words[i];
+//         if (str.includes(word)) {
+//                hasSpam = true;
+//         break;
+//         }
+         
+//     }
 
-let students = 100;
-students += 50;
-console.log(students);
+//     return hasSpam;
+// }
 
-let x = 0;
-x++;
-console.log(--x);
+// console.log(checkForSpam('SPam sapdfasfasf'));
+// console.log(checkForSpam('SPaf'));
+// console.log(checkForSpam('SALE sapdfasfasf'));
+// console.log(checkForSpam('hui hui hui '));
 
-// Example 3 - Приоритет операторов
-// Разбери приоритет операторов в инструкции присвоения значения переменной result.
+/// ===== login check
 
-const result = 108 + 223 - 2 * 5;
-console.log(result);
+// const checkForLogin = (userLogin) => {
+//     const logins = ['admin', 'root', 'user'];
+    
+//     let hasLogin = false;
+//     for (const login of logins) {
+//         if (userLogin === login) {
+//             hasLogin = true;
+//         }
+//     }
 
+//     return hasLogin;
+// }
 
-// Example 4 - Класс Math
-// Напиши скрипт, который выводит в консоль округленные вверх/вниз и т.д. значения переменной value.
-//  Используй методы Math.floor(), Math.ceil() и Math.round().Проверь что будет в консоли при значениях 27.3 и 27.9.
-
-const value = 27.5;
-console.log(Math.floor(value));
-console.log(Math.ceil(value));
-console.log(Math.round(value));
-
-console.log(~~value) // == 
-
-// Example 5 - Шаблонные строки
-// Составь фразу с помощью шаблонных строк A has B bots in stock, где A, B - переменные вставленные в строку.
-
-// const companyName = 'Cyberdyne Systems';
-// const repairBots = 150;
-// const defenceBots = 50;
-// const message = ``;
-// console.log(message); // "Cyberdyne Systems has 200 bots in stock"
-
-const companyName = 'Cyberdyne Systems';
-const repairBots = 150;
-const defenceBots = 50;
-const message = `Cyberdyne Systems has ${repairBots + defenceBots} bots in stock`;
-console.log(message)
-
-// Example 6 - Методы строк и чейнинг
-// Напиши скрипт который рассчитывает индекс массы тела человека. Для этого необходимо разделить вес в киллограммах 
-// на квадрат высоты человека в метрах.
-
-// Вес и высота хранятся в переменных weight и height, но не как числа, а в виде строк (специально для задачи).
-//  Нецелые числа могут быть заданы в виде 24.7 или 24, 7, то есть в качестве разделителя дробной части может быть запятая.
-
-// Индекс массиы тела необходимо округлить до одной цифры после запятой;
-
-let weight = '88,3';
-let height = '1.75';
-
-
-const bmi = Number.parseFloat(weight.replace(',','.')) / Number.parseFloat(Math.pow(height, 2)) ;
-console.log(bmi.toFixed(1)); // 28.8
-
-
-// Example 7 - Операторы сравнения и приведение типов
-// Каким будет результат выражений?
-
-console.log(5 > 4);
-
-console.log(10 >= '7');
-
-console.log('2' > '12');
-
-console.log('2' < '12');  // string
-
-console.log('4' == 4);
-
-console.log('6' === 6);
-
-console.log('false' === false);
-
-console.log(1 == true);
-
-console.log(1 === true);
-
-console.log('0' == false);
-
-console.log('0' === false);
-
-console.log('Papaya' < 'papaya');
-
-console.log('Papaya' === 'papaya');
-
-console.log(undefined == null);
-
-console.log(null === null);
-
-
-console.log('Example 8 - Логические операторы')
-
-// Каким будет результат выражений?
-
-console.log(true && 3);
-
-console.log(false && 3);
-
-console.log(true && 4 && 'kiwi');
-
-console.log(true && 0 && 'kiwi');
-
-console.log(true || 3);
-
-console.log(true || 3 || 4);
-
-console.log(true || false || 7);
-
-console.log(null || 2 || undefined);
-
-console.log((1 && null && 2) > 0);
-
-console.log(null || (2 && 3) || 4);
-
-
-// Example 9 - Значение по умолчанию и оператор нулевого слияния
-// Отрефактори код так, чтобы в переменную value присваивалось значение переменной incomingValue, 
-// если оно не равно undefined или null.В противном случае должно присваиваться значение defaultValue.
-// Проверь работу скрипта для слепдующих значений переменной incomingValue: null, undefined, 0, false.
-// Используй оператор ?? (nullish coalescing operator).
-
-const incomingValue = null;
-const defaultValue = 10;
-const value1 = incomingValue || defaultValue;
-console.log(value1);
-
-
-
-// Example 10 - Опертор % и методы строк
-// Напиши скрипт который переведёт значение totalMinutes (количество минут) в строку в формате часов и минут HH:MM.
-
-// 70 покажет 01:10
-// 450 покажет 07:30
-// 1441 покажет 24:01
-const totalMinutes = 450;
-
-const hours = Math.floor(totalMinutes / 60);
-const minutes = totalMinutes % 60;
-console.log(hours);
-console.log(minutes);
-
-const doubleDigitHours = String(hours).padStart(2, 0);
-const doubleDigitMinutes = String(minutes).padStart(2, 0);
-console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+// console.log(checkForLogin('user'));
+// console.log(checkForLogin('root'));
+// console.log(checkForLogin('admhuiin'));
+// console.log(checkForLogin('user'));
