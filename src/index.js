7ab0ddec828d8df8bc7a0d3e0f025df6a46dfdfc
sergiilevitan <84475017+sergiilevitan@users.noreply.github.com>
2022-01-11 22:16:34 +0200

    end of "this" lesson

--- src/index.js
+++ src/index.js
@@ -1,117 +1,142 @@
-console.log('module 4.1');
+
+
+console.log('module 5.1');
+
+
+// import { cars } from './cars.js';
+// function sayHello(a, b) {
+//     console.log(this, a+b)
+// }
+
+
+// const obj1 = { name: 'Bobby' };
+// const obj2 = { name: 'Kitty' };
+
+// // const fn1 = sayHello.bind(obj1)
+
+// // console.log(fn1 (12,13))
+
+// function myBind(context, callback) {
+//     return function () {
+//         callback.apply(context, arguments);
+//     };
+// }
+
+// const fn2 = myBind(obj1, sayHello);
+
+// fn2(12,22) // name Bobby
+
+// // setTimeout(fn2, 4000)
+
+// const customer = {
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//     },
   
-// === advanced reduce method ===
+// };
 
-// const tweets = [
-//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
-//   { id: "001", likes: 2, tags: ["html", "css"] },
-//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-//   { id: "003", likes: 8, tags: ["css", "react"] },
-//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-// ];
+// // function makeMessage(callback) {
+// //   // callback() это вызов метода getFullName без объекта
+// //   console.log(`Обрабатываем заявку от ${callback()}.`);
+// // }
 
-// // console.log(tweets.reduce((acc, item) => acc + item.likes, 0))
+// console.log(customer.getFullName()); // Будет ошибка при вызове функции
 
-// const flatTagsTogether = (acc, { tags }) => [...acc, ...tags];
-// const countValues = (acc, item) => ({
-//     ...acc,
-//     [item]: acc[item] ? acc[item] + 1 : 1,
-// });
 
-// console.log(tweets.reduce(flatTagsTogether, []).reduce(countValues,{}))
+// Example 1 - Мастерская драгоценностей
+// Напишите метод calcTotalPrice(stoneName), который принимает название камня и рассчитывает
+//  и возвращает общую стоимость камней с таким именем, ценой и количеством из свойства stones.
 
-// ==== Example 1 - Метод map
+// const chopShop = {
+//   stones: [
+//     { name: 'Emerald', price: 1300, quantity: 4 },
+//     { name: 'Diamond', price: 2700, quantity: 3 },
+//     { name: 'Sapphire', price: 1400, quantity: 7 },
+//     { name: 'Ruby', price: 800, quantity: 2 },
+//   ],
+//     calcTotalPrice(stoneName) {
+//         const { price, quantity } = this.stones.find(({ name }) => name === stoneName);
+//         return price * quantity
 
-import { cars } from './cars.js';
+//   },
+// };
 
-// ==== Пусть функция getModels возвращает массив моделей (поле model) всех автомобилей.
+// console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+// console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+// console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+// console.log(chopShop.calcTotalPrice('Ruby')); // 1600
 
-// const getModels = cars => cars.map((car) => car.model)
 
-// console.table(getModels(cars));
+// // Example 2 - Телефонная книга
+// // Выполните рефакторинг методов объекта phonebook чтобы код заработал.
 
-// == Пусть функция makeCarsWithDiscount возвращает новый массив объектов с изменным значением свойства price в зависимости от переданной скидки.
+// const phonebook = {
+//   contacts: [],
+//   add(contact) {
+//     const newContact = {
+//       list: 'default',
+//       ...contact,
+//       id: this.generateId(),
+//       createdAt: this.getDate(),
+//     };
+//     this.contacts.push(newContact);
+// },
+  
+//   generateId() {
+//     return '_' + Math.random().toString(36).substr(2, 9);
+//     },
+  
+//   getDate() {
+//     return Date.now();
+//   },
+// };
 
-// const makeCarsWithDiscount = (cars, discount) =>
-//     cars.map((car) => ({
-//         ...car,
-//         price: car.price - car.price * discount
-//     })
+// console.log(
+//   phonebook.add({
+//     name: 'Mango',
+//     email: 'mango@mail.com',
+//     list: 'friends',
+//   }),
+// );
+// console.log(
+//   phonebook.add({
+//     name: 'Poly',
+//     email: 'poly@hotmail.com',
+//   }),
+    
+//     console.log(phonebook.contacts)
 // );
 
-// console.table(makeCarsWithDiscount(cars, 0.2));
-// console.table(makeCarsWithDiscount(cars, 0.4));
-
-// ====== Пусть функция filterByPrice возвращает массив автомобилей цена которых меньше чем значение параметра threshold.
-
-// const filterByPrice = (cars, threshold) =>  cars.filter((car) => car.price < threshold)
-// ;
-
-// console.table(filterByPrice(cars, 30000));
-// console.table(filterByPrice(cars, 25000));
-
-// ===Пусть функция getCarsWithDiscount возвращает массив автомобилей свойство onSale которых true.
-
-// const getCarsWithDiscount = cars => cars.filter(({onSale}) => onSale);
-
-// console.table(getCarsWithDiscount(cars));
-
-// ==== Example 5 - Метод filter Пусть функция getCarsWithType возвращает массив автомобилей тип которых совпадает со значением параметра type.
-
-// const getCarsWithType = (cars, type) =>
-//     cars.filter((car) => car.type === type );
-
-// console.table(getCarsWithType(cars, 'suv'));
-// console.table(getCarsWithType(cars, 'sedan'));
-
-// const getCarByModel = (cars, model) => cars.find(({model: carModel})  => carModel === model);
-
-// console.log(getCarByModel(cars, 'F-150'));
-// console.log(getCarByModel(cars, 'CX-9'));
-
-// ==== Пусть функция sortByAscendingAmount возвращает новый массив автомобилей отсортированный по возврастанию значения свойства amount.
-
-// const sortByDescendingPrice = cars =>
-//     [...cars].sort((car1,car2) => car1.amount - car2.amount);
-
-// console.table(sortByDescendingPrice(cars));
-
-//  =====  Пусть функция sortByModel возвращает новый массив автомобилей отсортированный по названию модели в алфавитном и обратном алфавитном порядке, в засисимости от значения параметра order.
-
-// const sortByModel = (cars, order) =>
-//     [...cars].sort((car1, car2) =>
-//         order === 'desc'
-//         ? car2.model.localeCompare(car1.model)
-//         : car1.model.localeCompare(car2.model)
-//     )
-// ;
-
-// console.table(sortByModel(cars, 'asc'));
-// console.table(sortByModel(cars, 'desc'));
-
-// ==== Reduce Пусть функция getTotalAmount возвращает общее количество автомобилей(значение свойств amount).
-
-// const getTotalAmount = (cars) => cars.reduce((total, car) => total + car.amount, 0);
-
-// console.log(getTotalAmount(cars));
-
-// ==== Пусть функция getAvailableCarNames возвращает массив моделей автомобилей, но только тех, которые сейчас на распродаже.
-
-// const getModelsOnSale = cars => cars
-//     .filter(({ onSale }) => onSale)
-//     .map(({ model }) => model);
 
 
-// // ====== Пусть функция getSortedCarsOnSale возвращает массив автомобилей на распродаже (свойство onSale), отсортированных по возрастанию цены.
+// // Example 3 - Калькулятор
+// // Создайте объект calculator с тремя методами:
 
-// const getSortedCarsOnSale = cars => cars
-//     .filter(({ onSale }) => onSale)
-//     .sort((car1,car2) => car1.price - car2.price)
+// read(a, b)-  // принимает два значения и сохраняет их как свойства объекта.
+// add() - // возвращает сумму сохранённых значений.
+// //     mult() -  // перемножает сохранённые значения и возвращает результат.
 
-// console.table(getSortedCarsOnSale(cars));
+// const calculator = {
+//     read(a, b) {
+//         this.a = a;
+//         this.b = b;
+//     },
 
-// const str = 'hello super world'
+//     add() {
+//         return this.a + this.b;
+//     },
+//     mult() {
+//         return this.a * this.b;
+//     }
+// };
 
-// const reverseWord = (text) => text.split(' ').map((word) => word.split('').reverse().join('')).join(' ');
+// console.log(calculator)
 
-// console.log(reverseWord(str))
+// calculator.read(2, 3)
+
+// console.log(calculator)
+
+// console.log(calculator.add());
+// console.log(calculator.mult())
