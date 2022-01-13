685e2925a2e7208e9c8d23dcb39f179f7bf3f6f4
sergiilevitan <84475017+sergiilevitan@users.noreply.github.com>
2022-01-13 21:14:39 +0200

    modal window events

--- src/lection.js
+++ src/lection.js
@@ -1,1 +1,17 @@
 import { cars } from './cars.js';
+
+const output = document.querySelector('.js-output');
+const clearBtn = document.querySelector('.js-clear');
+
+window.addEventListener('keydown', keyDown);
+clearBtn.addEventListener('click', onClearOutput )
+
+function keyDown(evt) {
+    output.textContent += evt.key
+}
+
+function onClearOutput() {
+    
+    output.textContent = ' ';
+    
+}
